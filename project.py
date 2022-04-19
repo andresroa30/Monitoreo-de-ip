494be1092e752540fffd5780b34fb6d3ad113446
D4c <57877915+D4c24@users.noreply.github.com>
2022-04-19 19:21:15 +0400

    Update project.py
    Add the code for monitoring and sending messages to telegram chat bot

--- project.py
+++ project.py
@@ -1,5 +1,28 @@
+import os, requests, telegram
+from telegram.ext import Updater
+
+##HTTP API:
+ip_list = ['8.8.8.8', '8.8.4.4'] ##Set a list of the IP addresses or range IP you want to monitor
+user_id = 'userid' ##set the user ID from your telegram bot
+api_key = 'apikey' ##Set API Key
+
+## function to send the messge to telegram
+def bot(msg):
+  bot = telegram.Bot(token=api_key)
+  bot.send_message(chat_id=user_id, text=str(msg))
+
+## Function to monitor the IP list set before
 def run():
-  pass
-  
+  for ip in ip_list:
+    response = os.popen(f"ping {ip}").read()
+    if "recibidos = 4" in response:
+      msg = "UP " + str(ip) + " ping successful"
+      print(msg)
+      response = bot(msg)
+    else:
+      msg = "Down " + str(ip) + " Ping unsuccessful"
+      print(msg)
+      response = bot(msg)
+
 if __name__ == '__main__':
   run()
